3b085ae7df273d14c794cbdaee5a579b86857bfe
Clark Oake <clarkoake13@gmail.com>
2023-08-31 19:14:42 -0230

    refactoring and suggestions
    added some suggestions for handling submission of form values and retrieving related workload-value attributes on those elements that have predetermined workload scores
    
    Also refactored the handling of displaying form section sbased on tabs using a JS callback function
    
    added styling to improve the layout of the app's header and form tabs

--- script.js
+++ script.js
@@ -7,55 +7,101 @@
 // Break things into small pieces as much as possible and test often (console.log() and error messages)
 // small changes are easier to debug than building half your app and finding out you went astray somewhere
 
- //Adds functionality to Bed links
+//Adds functionality to Bed links
 document.addEventListener("DOMContentLoaded", function () {
-    const bedLinks = document.querySelectorAll(".bed-link");
-    const formFieldsets = document.querySelectorAll(".workload-form fieldset");
+  const bedLinks = document.querySelectorAll(".bed-link");
+  const formFieldsets = document.querySelectorAll(".workload-form fieldset");
 
-    bedLinks.forEach(function (bedLink) {
-        bedLink.addEventListener("click", function () {
-            formFieldsets.forEach(function (fieldset) {
-                fieldset.style.display = "none";
-            });
-            formFieldsets[0].style.display = "block";
-        })
-    });
+  bedLinks.forEach(function (bedLink) {
+    bedLink.addEventListener("click", function () {
+      formFieldsets.forEach(function (fieldset) {
+        fieldset.style.display = "none";
+      });
+      formFieldsets[0].style.display = "block";
+    })
+  });
 });
 
- // Adds functionality to form tabs
- const button1 = document.querySelector(".fieldset1");
- const fieldsetAssessCollab = document.querySelector(".assess-collab");
+// Adds functionality to form tabs
+const assess_collab_link = document.getElementById("assess-collab-tab");
+const fieldsetAssessCollab = document.getElementById("assess-collab-form");
 
- button1.addEventListener('click', () => {
-   fieldsetAssessCollab.classList.toggle("hidden");
- })
+const basic_care_link = document.getElementById('basic-care-tab');
+const fieldsetBasicCare = document.getElementById("basic-care-form");
+
+const monitor_eval_link = document.getElementById("monitor-eval-tab");
+const fieldsetMonitorEval = document.getElementById("monitor-eval-form");
+
+const nurse_int_link = document.getElementById("nurse-int-tab");
+const fieldsetNurseIntervention = document.getElementById("nurse-int-form");
+
+const other_int_link = document.getElementById("other-int-tab");
+const fieldsetOtherIntervention = document.getElementById("other-int-form");
+
+// Grabs all form sections based on a shared classname
+const allFormSections = Array.from(document.getElementsByClassName('fieldset'));
+
+// Generic callback for showing discrete form sections based on the tab being clicked
+const showFormSection = (formSectionToShow) =>
+{
+  if (!formSectionToShow) throw Error('Invalid HTML element provided as input')
+
+  // Hides all form sections to start with
+  allFormSections.forEach(formSection => formSection.classList.add('hidden'))
+
+  // show only the form section specified as input
+  formSectionToShow.classList.remove('hidden')
+}
+
+// shows related form section and hides all others
+assess_collab_link.addEventListener('click', () => showFormSection(fieldsetAssessCollab));
+basic_care_link.addEventListener('click', () => showFormSection(fieldsetBasicCare));
+monitor_eval_link.addEventListener('click', () => showFormSection(fieldsetMonitorEval))
+nurse_int_link.addEventListener('click', () => showFormSection(fieldsetNurseIntervention))
+other_int_link.addEventListener('click', () => showFormSection(fieldsetOtherIntervention))
+
+// ========================================================================
+// TEST SUBMIT CODE WITH WORKLOAD VALUE ATTRIBUTE
+// ========================================================================
+
+const test_btn = document.getElementById('test_btn');
+
+const assess_admit = document.getElementById('admit-assess');
+
+const meetings = document.getElementById('meetings');
+
+// Testing for submission of checkbox with predefined workload-value attached to element
+// test_btn.addEventListener('click', () => {
+//   // NOTE: There is a string literal in the first return here, it allows us to render JS code directly in a string which cannot
+//   // be done using normal single quotes ' ' 
+//   if(assess_admit.checked) return console.log(`Added ${assess_admit.getAttribute('workload-value')} workload points to the total`);
+
+//   else return console.log('no workload value is modified')
+// })
 
 
- const button2 = document.querySelector('.fieldset2');
- const fieldsetBasicCare = document.querySelector(".basic-care");
+const someValidationCallback = (input_value) => {
+  if (!input_value) throw Error(`no input value provided`);
 
- button2.addEventListener('click', () => {
-   fieldsetBasiccare.classList.toggle("hidden");
- })
+  if (typeof (input_value) !== 'number') throw Error(`Input value of ${input_value} is not valid as a number`)
 
- const button3 = document.querySelector(".fieldset3");
- const fieldsetMonitorEval = document.querySelector(".monitor-eval");
+  // other filter conditions can be added here
 
- button3.addEventListener('click', () => {
-   fieldsetMonitorEval.classList.toggle("hidden");
- })
-
- const button4 = document.querySelector(".fieldset4");
- const fieldsetNurseIntervention = document.querySelector(".nurse-intervention");
-
- button4.addEventListener('click', () => {
-   fieldsetNurseIntervention.classList.toggle("hidden");
- })
+  return input_value
+}
 
 
- const button5 = document.querySelector(".fieldset5");
- const fieldsetOtherIntervention = document.querySelector(".other-intervention");
+let workload_point_total = 0
 
- button5.addEventListener('click', () => {
-   fieldsetOtherIntervention.classList.toggle("hidden");
- })
+// Testing for submission of freeform number input w/ no predetermine workload-value attached
+test_btn.addEventListener('click', () => {
+  // NOTE: There is a string literal in the first return here, it allows us to render JS code directly in a string which cannot
+  // be done using normal single quotes ' ' 
+  if (meetings.value) {
+    const input_points_value = someValidationCallback(meetings.value)
+    workload_point_total = workload_point_total + input_points_value
+    return console.log(`Added ${meetings.value} workload points to the total`, `workload point total = ${workload_point_total}`);
+  }
+
+  else return console.log('no value entered in the meetings input')
+})
